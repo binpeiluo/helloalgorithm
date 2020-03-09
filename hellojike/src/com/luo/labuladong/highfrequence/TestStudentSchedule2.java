5e39e3983289009af9b24fb458c6cebb4f9436ad
binpeiluo <1881975167@163.com>
2020-03-09 17:59:11 +0800

    高频问题 学生座位调度问题

--- hellojike/src/com/luo/labuladong/highfrequence/TestStudentSchedule2.java
+++ hellojike/src/com/luo/labuladong/highfrequence/TestStudentSchedule2.java
@@ -7,94 +7,99 @@ import java.util.*;
  *      学生座位调度,挺有意思
  */
 public class TestStudentSchedule2 {
-    // 将端点 p 映射到以 p 为左端点的线段
-    private Map<Integer, int[]> startMap;
-    // 将端点 p 映射到以 p 为右端点的线段
-    private Map<Integer, int[]> endMap;
-    // 根据线段⻓度从⼩到⼤存放所有线段
-    private TreeSet<int[]> pq;
-    private int N;
+
+    private int n;
+    private TreeSet<int[]> treeSet;
+    private Map<Integer,int[]> startMap;
+    private Map<Integer,int[]> endMap;
 
     public TestStudentSchedule2(int N){
-        this.N = N;
-        startMap = new HashMap<>();
-        endMap = new HashMap<>();
-        pq = new TreeSet<>((a, b) -> {
-            int distA = distance(a);
-            int distB = distance(b);
-            // 如果⻓度相同，就⽐较索引
-            if (distA == distB)
-                return b[0] - a[0];
-            return distA - distB;
+        this.n=N;
+        treeSet=new TreeSet<>((a,b)->{
+            int distanceA=distance(a);
+            int distanceB=distance(b);
+            if(distanceA==distanceB)
+                return b[0]-a[0];
+            return distanceA-distanceB;
         });
-        // 在有序集合中先放⼀个虚拟线段
-        addInterval(new int[] {-1, N});
+        startMap=new HashMap<>();
+        endMap=new HashMap<>();
+        treeSet.add(new int[]{-1,N});
     }
-    /* 去除⼀个线段 */
-    private void removeInterval(int[] intv) {
-        pq.remove(intv);
-        startMap.remove(intv[0]);
-        endMap.remove(intv[1]);
-    }
-    /* 增加⼀个线段 */
-    private void addInterval(int[] intv) {
-        pq.add(intv);
-        startMap.put(intv[0], intv);
-        endMap.put(intv[1], intv);
-    }
-    /* 计算⼀个线段的⻓度 */
-    private int distance(int[] intv) {
-        int x = intv[0];
-        int y = intv[1];
-        if (x == -1) return y;
-        if (y == N) return N - 1 - x;
-        // 中点和端点之间的⻓度
-        return (y - x) / 2;
-    }
+
+    /**
+     * 学生找到座位坐下,可以视为将最长一条线段二分切分为两条.
+     * @return
+     */
     public int seat() {
-// 从有序集合拿出最⻓的线段
-        int[] longest = pq.last();
-        int x = longest[0];
-        int y = longest[1];
         int seat;
-        if (x == -1) { // 情况⼀
-            seat = 0;
-        } else if (y == N) { // 情况⼆
-            seat = N - 1;
-        } else { // 情况三
-            seat = (y - x) / 2 + x;
+        int[] last = treeSet.last();
+        if(last[0]==-1)
+            seat=0;
+        else if(last[1]==n)
+            seat=n-1;
+        else{
+            seat=last[0]+(last[1]-last[0])/2;
         }
-// 将最⻓的线段分成两段
-        int[] left = new int[] {x, seat};
-        int[] right = new int[] {seat, y};
-        removeInterval(longest);
-        addInterval(left);
-        addInterval(right);
+        removeInterval(last);
+        addInterval(new int[]{last[0],seat});
+        addInterval(new int[]{seat,last[1]});
         return seat;
     }
-    public void leave(int p) {
-// 将 p 左右的线段找出来
 
-        int[] right = startMap.get(p);
+    /**
+     * 学生离开座位可以抽象为将两条线段合并为一条
+     * @param p
+     */
+    public void leave(int p) {
+        int[] right=startMap.get(p);
         int[] left = endMap.get(p);
-// 合并两个线段成为⼀个线段
-        int[] merged = new int[] {left[0], right[1]};
-        removeInterval(left);
         removeInterval(right);
-        addInterval(merged);
+        removeInterval(left);
+        addInterval(new int[]{left[0],right[1]});
+    }
+
+    private void removeInterval(int[] interval){
+        treeSet.remove(interval);
+        startMap.remove(interval[0]);
+        endMap.remove(interval[1]);
+    }
+
+    private void addInterval(int[] interval){
+        treeSet.add(interval);
+        startMap.put(interval[0],interval);
+        endMap.put(interval[1],interval);
+    }
+
+    /**
+     * 计算线段的距离,在这里距离3和4是一样的
+     * @param interval
+     * @return
+     */
+    private int distance(int[] interval){
+        return (interval[1]-interval[0]-1+1)/2;
     }
 
     public static void main(String[] args){
         TestStudentSchedule2 test=new TestStudentSchedule2(10);
         test.seat();
+        printTreeSet(test.treeSet);
         test.seat();
+        printTreeSet(test.treeSet);
         test.seat();
+        printTreeSet(test.treeSet);
         test.seat();
+        printTreeSet(test.treeSet);
         test.leave(4);
+        printTreeSet(test.treeSet);
         test.seat();
+        printTreeSet(test.treeSet);
 
-        System.out.println(test.pq);
-        test.pq.stream().forEach(interval->{
+    }
+
+    private static void printTreeSet(TreeSet<int[]> treeSet){
+        System.out.println("printTreeSet");
+        treeSet.stream().forEach(interval->{
             System.out.println(interval[0]+" - "+interval[1]);
         });
     }
