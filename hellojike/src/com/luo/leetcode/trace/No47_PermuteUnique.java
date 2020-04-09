15c3241ab644494d8f5d570333de56451c75d451
binpeiluo <1881975167@163.com>
2020-04-09 18:46:58 +0800

    算法思维 回溯思维 含有重复元素的全排列

--- hellojike/src/com/luo/leetcode/trace/No47_PermuteUnique.java
+++ hellojike/src/com/luo/leetcode/trace/No47_PermuteUnique.java
@@ -46,11 +46,51 @@ public class No47_PermuteUnique {
         }
     }
 
+    /**
+     * 由于有重复元素,首选对数组进行排序,然后使用visited数组辅助判断
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        helper2(result,new ArrayList<>(),visited,nums);
+        return result;
+    }
+
+    private void helper2(List<List<Integer>> result,List<Integer> currSelected,
+                         boolean[] visited,int[] nums){
+        if(currSelected.size()==nums.length){
+            result.add(new ArrayList<>(currSelected));
+            return ;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 剪枝条件：i > 0 是为了保证 nums[i - 1] 有意义
+            // 写 !used[i - 1] 是因为 nums[i - 1] 在深度优先遍历的过程中刚刚被撤销选择
+            //正是因为刚被撤销，下面的搜索中还会使用到，因此会产生重复，剪掉的就应该是这样的分支
+            if((i>0&&nums[i]==nums[i-1])&&!visited[i-1])
+                continue;
+            if(visited[i])
+                continue;
+            currSelected.add(nums[i]);
+            visited[i]=true;
+            helper2(result,currSelected,visited,nums);
+            currSelected.remove(currSelected.size()-1);
+            visited[i]=false;
+
+        }
+    }
+
     public static void main(String[] args){
         No47_PermuteUnique test=new No47_PermuteUnique();
-        int[] nums={1,1,1,2,2,2};
+//        int[] nums={1,1,1,2,2,2};
+        int[] nums={1,1,2,3};
         List<List<Integer>> lists = test.permuteUnique(nums);
         System.out.println(lists);
         System.out.println(lists.size());
+
+        List<List<Integer>> lists1 = test.permuteUnique2(nums);
+        System.out.println(lists1);
     }
 }
