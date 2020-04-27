2056d056ffcf10bcbfb3eb154fc4b6a7953977cb
luopeibin <18819475167@163.com>
2020-04-27 23:14:38 +0800

    数组系列 二分查找  33. 搜索旋转排序数组

--- hellojike/src/com/luo/leetcode/array/No33_Search.java
+++ hellojike/src/com/luo/leetcode/array/No33_Search.java
@@ -115,14 +115,54 @@ public class No33_Search {
         return -1;
     }
 
+    /**
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search4(int[] nums,int target){
+        int len=nums.length;
+        if(len==0)
+            return -1;
+        int left=0,right=len-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+                return mid;
+//            前半部分有序
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left]&&target<=nums[mid]){
+                    right=mid;
+                }else{
+                    left=mid+1;
+                }
+            }
+//            后半部分有序
+            else{
+                if(target>=nums[mid]&&target<=nums[right]){
+                    left=mid;
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+
     public static void main(String[] args){
         No33_Search test=new No33_Search();
         int[] nums={4,5,6,7,0,1,2};
-        int target=0;
+        int target=1;
         int i = test.search2(nums, target);
         System.out.println(i);
 
         int i1 = test.search3(nums, target);
         System.out.println(i1);
+
+        int i2 = test.search4(nums, target);
+        System.out.println(i2);
     }
 }
