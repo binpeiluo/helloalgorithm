581271b53e25cd2bd6a390143d38865b4caafa22
luopeibin <18819475167@163.com>
2020-05-24 17:44:11 +0800

    滑动窗口系列  寻找最长无重复子串长度

--- hellojike/src/com/luo/algorithm/doublepointer/slidepointer/LengthOfLongestSubstring.java
+++ hellojike/src/com/luo/algorithm/doublepointer/slidepointer/LengthOfLongestSubstring.java
@@ -37,11 +37,33 @@ public class LengthOfLongestSubstring {
         return result;
     }
 
+
+    public int lengthOfLongestSubstring2(String s){
+        Map<Character,Integer> window=new HashMap<>();
+        int result=0;
+        int left=0,right=0;
+        while(right<s.length()){
+            char c = s.charAt(right);
+            window.put(c,window.getOrDefault(c,0)+1);
+            right++;
+            while(window.get(c)>1){
+                char d = s.charAt(left);
+                window.put(d,window.getOrDefault(d,0)-1);
+                left++;
+            }
+            result=Math.max(result,(right-left));
+        }
+        return result;
+    }
+
     public static void main(String[] args){
         LengthOfLongestSubstring test=new LengthOfLongestSubstring();
         String s="abcabcbb";
 
         int i = test.lengthOfLongestSubstring(s);
         System.out.println(i);
+
+        int i1 = test.lengthOfLongestSubstring2(s);
+        System.out.println(i1);
     }
 }
