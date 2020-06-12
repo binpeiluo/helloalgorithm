3de4ba621fd1d5eee22ba992aeb670eafa67a24e
luopeibin <18819475167@163.com>
2020-06-12 12:48:30 +0800

    三数之和

--- hellojike/src/com/luo/leetcode/array/No15_ThreeSum.java
+++ hellojike/src/com/luo/leetcode/array/No15_ThreeSum.java
@@ -133,11 +133,42 @@ public class No15_ThreeSum {
     }
 
 
+    public List<List<Integer>> threeSum5(int[] nums){
+        Arrays.sort(nums);
+        int len=nums.length;
+        List<List<Integer>> result=new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            if(nums[i]>0){
+                break;
+            }
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int left=i+1,right=len-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if((left>i+1&&nums[left]==nums[left-1])||sum<0){
+                    left++;
+                }else if((right<len-1&&nums[right-1]==nums[right])||sum>0){
+                    right--;
+                }else{
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
 
     public static void main(String[] args){
         No15_ThreeSum test=new No15_ThreeSum();
         int[] nums={2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4};
         List<List<Integer>> lists = test.threeSum3(nums);
         System.out.println(lists);
+
+        List<List<Integer>> lists1 = test.threeSum5(nums);
+        System.out.println(lists1);
     }
 }
