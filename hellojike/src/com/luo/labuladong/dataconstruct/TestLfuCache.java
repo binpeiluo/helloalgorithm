cb4db3a336a77b1e5a28098b7320e60866f203ef
binpeiluo <1881975167@163.com>
2020-12-04 15:00:31 +0800

    LFU算法

--- hellojike/src/com/luo/labuladong/dataconstruct/TestLfuCache.java
+++ hellojike/src/com/luo/labuladong/dataconstruct/TestLfuCache.java
@@ -1,6 +1,7 @@
 package com.luo.labuladong.dataconstruct;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * LFU
@@ -8,287 +9,301 @@ import java.util.*;
 @SuppressWarnings("Duplicates")
 public class TestLfuCache {
 
-    /**
-     * navie想法,使用三个map分别保存 k-v,k-f,f-ks之间的关系,
-     * 并且记录 minFreq.
-     * 这里 f-ks 映射使用 TreeMap<Integer,<LinkedHashSet>>
-     *  使用TreeMap是为了将key有序组织起来,方便找到最小的minFreq.
-     *  由于使用红黑树,这里时间复杂度为 O(logn),其实不太符合题目要求O(1)
-     *
-     *  都使用了 minFreq保存最小访问次数,那这里还有需要使用 TreeMap来有序地组织key吗?
-     *  其实duck不必.仔细思考,当插入新纪录后,minFreq必定为1.
-     *  只有当需要插入记录才会删除记录.所以只需要保证在删除记录的时候 minFreq是正确的即可.
-     *  删除记录前的操作,有可能为获取了记录,此时更新逻辑一定正确
-     */
     static class LfuCache{
-
-//        key->value
-        Map<Integer,Integer> cache=new HashMap<>();
-//        key->k2f
-        Map<Integer,Integer> visited=new HashMap<>();
-//        k2f->key list
-//        LinkedHashMap 能通过访问次序调整位置
-//        TreeMap 数据结构为红黑树,可以将key按照大小顺序组织,但获取key的复杂度为O(logn)级别
-        Map<Integer, LinkedHashSet<Integer>> visitedList=new TreeMap<>();
-//        最小访问次数
-        int minVisitedCnt =Integer.MIN_VALUE;
-        int capacity;
-
+        /** 储存键值对*/
+        private Map<Integer,Node> map;
+        /** 储存每个访问次序的链表*/
+        private Map<Integer,DoubleList> freq2Key;
+        private Map<Integer,Integer> key2Freq;
+        private int capacity;
+        private int count;
         LfuCache(int capacity){
             this.capacity=capacity;
+            this.count=0;
+            map=new HashMap<>();
+            freq2Key=new HashMap<>();
+            key2Freq=new HashMap<>();
         }
-
         public int get(int key){
-            if(cache.containsKey(key)){
-                visitKey(key);
-                return cache.get(key);
-            }else{
+            Node node = map.get(key);
+            if(node==null){
                 return -1;
+            }else{
+                this.visitNode(node);
+                return node.value;
             }
         }
 
         public void put(int key,int value){
-            if(cache.containsKey(key)){
-                visitKey(key);
-                cache.put(key,value);
-            }else{
-                if(cache.size()==capacity){
-                    removeLastVersion();
-                }
-//                防止 capacity=0
-                if(cache.size()<capacity){
-                    addNewKv(key,value);
-                }
-            }
-        }
-
-//        删除访问次数最小的元素
-        private void removeLastVersion(){
-            boolean canRemove= minVisitedCnt !=Integer.MIN_VALUE;
-            if(!canRemove){
+            if(capacity==0){
+//                绝了  还有这案例
                 return;
             }
-//            找到最小访问次数对应的key列表中的首个
-            int key=visitedList.get(minVisitedCnt).iterator().next();
-//            将该最小访问次数对应的key列表删除该key
-            visitedList.get(minVisitedCnt).remove(key);
-            if(visitedList.get(minVisitedCnt).isEmpty()){
-                visitedList.remove(minVisitedCnt);
-            }
-//            在缓存中删除key
-            cache.remove(key);
-//            在key->访问次数map中删除key
-            visited.remove(key);
-//            更新最小访问次数
-            updateMinVisitedCnt();
-        }
-
-        private void visitKey(int key){
-//            获取该key对应的访问次数
-            int currVisitedCnt=visited.get(key);
-//            计算新的访问次数
-            int newVisitedCnt=currVisitedCnt+1;
-//            在key->访问次数的map更新
-            visited.put(key,newVisitedCnt);
-//            在访问次数->key列表中删除该key,因为其对应的访问次数已经更新了
-            visitedList.get(currVisitedCnt).remove(key);
-            if(visitedList.get(minVisitedCnt).isEmpty()){
-                visitedList.remove(minVisitedCnt);
-            }
-//            更新key对应的访问次数
-            LinkedHashSet<Integer> orDefault = visitedList.getOrDefault(newVisitedCnt, new LinkedHashSet<>());
-            orDefault.add(key);
-            visitedList.put(newVisitedCnt,orDefault);
-//            更新最小访问次数
-            updateMinVisitedCnt();
-        }
-
-        private void addNewKv(int key,int value){
-            cache.put(key,value);
-            visited.put(key,1);
-            LinkedHashSet<Integer> orDefault = visitedList.getOrDefault(1, new LinkedHashSet<>());
-            orDefault.add(key);
-            visitedList.put(1,orDefault);
-//            更新最小访问次数
-            updateMinVisitedCnt();
-        }
-
-//        更新最小访问次数变量
-        private void updateMinVisitedCnt(){
-            Set<Integer> integers = visitedList.keySet();
-            if(integers.isEmpty()){
-                minVisitedCnt=Integer.MIN_VALUE;
+            Node node = map.get(key);
+            if(node==null){
+                if(capacity==count){
+                    this.removeOldestNode();
+                }
+                this.addNode(new Node(key,value));
             }else{
-                minVisitedCnt=integers.iterator().next();
+                node.value=value;
+                visitNode(node);
             }
+        }
+
+        /** 新添加节点*/
+        private void addNode(Node node){
+            int defaultFreq=1;
+//            访问频次到列表
+            DoubleList orDefault = freq2Key.getOrDefault(defaultFreq, new DoubleList());
+            orDefault.addLast(node);
+            freq2Key.put(defaultFreq,orDefault);
+//            键访问次数
+            key2Freq.put(node.key,defaultFreq);
+            map.put(node.key,node);
+            count++;
+        }
+
+        private void visitNode(Node node){
+            Integer freq = key2Freq.get(node.key);
+//            处理原来相同访问频率的链表 从中删除
+            DoubleList freqList = freq2Key.get(freq);
+            freqList.removeNode(node);
+            if(freqList.count()==0){
+                freq2Key.remove(freq);
+            }
+//            将之提升到另一个链表中
+            freq+=1;
+            DoubleList orDefault = freq2Key.getOrDefault(freq, new DoubleList());
+            orDefault.addLast(node);
+            freq2Key.put(freq,orDefault);
+
+//            修改频率
+            key2Freq.put(node.key,freq);
+        }
+
+        private Node removeOldestNode(){
+//            找到最小的频率
+            Integer minFreq = freq2Key.keySet().stream().min(Comparator.naturalOrder()).get();
+            DoubleList doubleList = freq2Key.get(minFreq);
+//            移除最小频率链表的头结点
+            Node node = doubleList.removeFirst();
+            if(doubleList.count()==0){
+                freq2Key.remove(minFreq);
+            }
+//            同时移除频率map
+            key2Freq.remove(node.key);
+//            同时删除键值对
+            map.remove(node.key);
+            count--;
+            return node;
+        }
+
+        private void printNode(){
+            System.out.println("-----begin freq2Key");
+            String collect = freq2Key.entrySet().stream().map(entry -> {
+                return "频率:" + entry.getKey() + " 链表:" + entry.getValue().toString();
+            }).collect(Collectors.joining("\r\n"));
+            System.out.println(collect);
+
+
+            System.out.println("-----begin key2Freq");
+            System.out.println(key2Freq.toString());
+
+
+        }
+
+    }
+
+    static class Node{
+        private int key,value;
+        private Node next,prev;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
-    static class LfuCacheV2{
 
-        //        key->value
-        Map<Integer,Integer> k2v ;
-        //        key->freq
-        Map<Integer,Integer> k2f ;
-        //        freq->keys
-        Map<Integer, LinkedHashSet<Integer>> f2ks ;
-        //        最小访问次数
-        int minFreq =Integer.MIN_VALUE;
-        int capacity;
-
-        LfuCacheV2(int capacity){
-            this.capacity=capacity;
-            k2v =new HashMap<>();
-            k2f =new HashMap<>();
-            f2ks =new HashMap<>();
+    static class DoubleList{
+        private Node head,tail;
+        private int count;
+        DoubleList(){
+            head=new Node(-1,-1);
+            tail=new Node(-1,-1);
+            head.next=tail;
+            tail.prev=head;
+            this.count=0;
         }
-
-        public int get(int key){
-            if(k2v.containsKey(key)){
-                visitKey(key);
-                return k2v.get(key);
-            }else{
-                return -1;
+        public void addLast(Node n){
+            n.next=tail;
+            n.prev=tail.prev;
+            tail.prev.next=n;
+            tail.prev=n;
+            this.count++;
+        }
+        public Node removeFirst(){
+            if(head.next==tail){
+                return null;
             }
+            Node node=head.next;
+            removeNode(node);
+            return node;
+        }
+        private void removeNode(Node n){
+            n.prev.next=n.next;
+            n.next.prev=n.prev;
+            n.next=null;
+            n.prev=null;
+            this.count--;
+        }
+        public int count(){
+            return this.count;
         }
 
-        public void put(int key,int value){
-            if(k2v.containsKey(key)){
-                visitKey(key);
-                k2v.put(key,value);
-            }else{
-                if(k2v.size()==capacity){
-                    removeLastVersion();
-                }
-//                防止 capacity=0
-                if(k2v.size()<capacity){
-                    addNewKv(key,value);
-                }
+        @Override
+        public String toString() {
+            return printNode();
+        }
+
+        private String printNode(){
+            Node tmp=head.next;
+            String result="{";
+            while(tmp!=tail){
+                result+=(" -> "+tmp.key);
+                tmp=tmp.next;
             }
+            result+="}";
+            return result;
         }
-
-        //        删除访问次数最小的元素
-        private void removeLastVersion(){
-            boolean canRemove= minFreq !=Integer.MIN_VALUE;
-            if(!canRemove){
-                return;
-            }
-//            找到最小访问次数对应的key列表中的首个
-            int key= f2ks.get(minFreq).iterator().next();
-//            将该最小访问次数对应的key列表删除该key
-            f2ks.get(minFreq).remove(key);
-            if(f2ks.get(minFreq).isEmpty()){
-                f2ks.remove(minFreq);
-            }
-//            在缓存中删除key
-            k2v.remove(key);
-//            在key->访问次数map中删除key
-            k2f.remove(key);
-//            更新最小访问次数
-//            不更新起始无所谓,因为接下来插入新元素肯定会将最小频次最为1
-        }
-
-        private void visitKey(int key){
-//            获取该key对应的访问次数
-            int currVisitedCnt= k2f.get(key);
-//            计算新的访问次数
-            int newVisitedCnt=currVisitedCnt+1;
-//            在key->访问次数的map更新
-            k2f.put(key,newVisitedCnt);
-//            在访问次数->key列表中删除该key,因为其对应的访问次数已经更新了
-            f2ks.get(currVisitedCnt).remove(key);
-            if(f2ks.get(minFreq).isEmpty()){
-                f2ks.remove(minFreq);
-//            更新最小访问次数
-                minFreq=newVisitedCnt;
-            }
-//            更新key对应的访问次数
-            LinkedHashSet<Integer> orDefault = f2ks.getOrDefault(newVisitedCnt, new LinkedHashSet<>());
-            orDefault.add(key);
-            f2ks.put(newVisitedCnt,orDefault);
-        }
-
-        private void addNewKv(int key,int value){
-            k2v.put(key,value);
-            k2f.put(key,1);
-            LinkedHashSet<Integer> orDefault = f2ks.getOrDefault(1, new LinkedHashSet<>());
-            orDefault.add(key);
-            f2ks.put(1,orDefault);
-//            更新最小访问次数
-//            此时最小频率一定为1
-            minFreq=1;
-        }
-
     }
 
 
     public static void main(String[] args){
-//        LfuCache lfuCache=new LfuCache(2);
-//        lfuCache.put(1,1);
-//        lfuCache.put(2,2);
-//        int i = lfuCache.get(1);
-//        System.out.println(i);
-//        lfuCache.put(3,3);
-//        i = lfuCache.get(2);
-//        System.out.println(i);
-//        i = lfuCache.get(1);
-//        System.out.println(i);
+//        testDemo1();
 
-//        [[3],[2,2],[1,1],[2],[1],[2],[3,3],[4,4],[3],[2],[1],[4]]
-//        LfuCache lfuCache=new LfuCache(3);
-//        lfuCache.put(2,2);
-//        lfuCache.put(1,1);
-//        int i = lfuCache.get(2);
-//        System.out.println(i);
-//        i = lfuCache.get(1);
-//        System.out.println(i);
-//        i = lfuCache.get(2);
-//        System.out.println(i);
-//        lfuCache.put(3,3);
-//        lfuCache.put(4,4);
-////        开始不对劲
-//        i = lfuCache.get(3);
-//        System.out.println(i);
-//        i = lfuCache.get(2);
-//        System.out.println(i);
-//        i = lfuCache.get(1);
-//        System.out.println(i);
-//        i = lfuCache.get(4);
-//        System.out.println(i);
+//        testDemo2();
 
-//        [[2],[1,1],[2,2],[1],[3,3],[2],[3],[4,4],[1],[3],[4]]
-//        LfuCache lfuCache=new LfuCache(2);
-//        lfuCache.put(1,1);
-//        lfuCache.put(2,2);
-//        int i = lfuCache.get(1);
-//        System.out.println(i);
-//        lfuCache.put(3,3);
-//        i = lfuCache.get(2);
-//        System.out.println(i);
-//        i = lfuCache.get(3);
-//        System.out.println(i);
-//        lfuCache.put(4,4);
-//        i = lfuCache.get(1);
-//        System.out.println(i);
-//        i = lfuCache.get(3);
-//        System.out.println(i);
-//        i = lfuCache.get(4);
-//        System.out.println(i);
+//        testDemo3();
 
-//        [[0],[0,0],[0]]
-//        LfuCache lfuCache=new LfuCache(0);
-//        lfuCache.put(0,0);
-//        int i = lfuCache.get(0);
-//        System.out.println(i);
+        testDemo4();
+    }
 
-//        [[1],[2,1],[2],[3,2],[2],[3]]
-        LfuCacheV2 lfuCache=new LfuCacheV2(1);
+    private static void testDemo4(){
+//        ["LFUCache","put","put","get","get","put","get","get","get"]
+//        [[2],[2,1],[3,2],[3],[2],[4,3],[2],[3],[4]]
+
+        LfuCache lfuCache=new LfuCache(2);
+
         lfuCache.put(2,1);
-        int i = lfuCache.get(2);
-        System.out.println(i);
+        lfuCache.printNode();
+
         lfuCache.put(3,2);
+        lfuCache.printNode();
+
+        int i = lfuCache.get(3);
+        System.out.println(i);
+        lfuCache.printNode();
+
         i = lfuCache.get(2);
         System.out.println(i);
+        lfuCache.printNode();
+
+        lfuCache.put(4,3);
+        lfuCache.printNode();
+
+        i = lfuCache.get(2);
+        System.out.println(i);
+        lfuCache.printNode();
+
         i = lfuCache.get(3);
         System.out.println(i);
+        lfuCache.printNode();
+
+        i = lfuCache.get(4);
+        System.out.println(i);
+        lfuCache.printNode();
+    }
+
+    private static void testDemo3(){
+//        ["LFUCache","put","get"]
+//        [[0],[0,0],[0]]
+
+        LfuCache lfuCache=new LfuCache(0);
+
+        lfuCache.put(0,0);
+        lfuCache.printNode();
+
+        int i = lfuCache.get(0);
+        System.out.println(i);
+        lfuCache.printNode();
+
+    }
+
+    private static void testDemo2(){
+//        ["LFUCache","put","put","put","put","get"]
+//        [[2],[3,1],[2,1],[2,2],[4,4],[2]]
+
+        LfuCache lfuCache=new LfuCache(2);
+
+        lfuCache.put(3,1);
+        lfuCache.printNode();
+
+        lfuCache.put(2,1);
+        lfuCache.printNode();
+
+        lfuCache.put(2,2);
+        lfuCache.printNode();
+
+        lfuCache.put(4,4);
+        lfuCache.printNode();
+
+        int i = lfuCache.get(2);
+        System.out.println(i);
+        lfuCache.printNode();
+    }
+
+    private static void testDemo1(){
+
+//          ["LFUCache","put","put","get","put","get","get","put","get","get","get"]
+//          [[2],[1,1],[2,2],[1],[3,3],[2],[3],[4,4],[1],[3],[4]]
+
+        LfuCache lfuCache=new LfuCache(2);
+        lfuCache.put(1,1);
+        lfuCache.printNode();
+
+        lfuCache.put(2,2);
+        lfuCache.printNode();
+
+        int i = lfuCache.get(1);
+        System.out.println(i);
+        lfuCache.printNode();
+
+        lfuCache.put(3,3);
+        lfuCache.printNode();
+
+        i = lfuCache.get(2);
+        System.out.println(i);
+        lfuCache.printNode();
+
+        i = lfuCache.get(3);
+        System.out.println(i);
+        lfuCache.printNode();
+
+        lfuCache.put(4,4);
+        lfuCache.printNode();
+
+        i = lfuCache.get(1);
+        System.out.println(i);
+        lfuCache.printNode();
+
+        i = lfuCache.get(3);
+        System.out.println(i);
+        lfuCache.printNode();
+
+        i = lfuCache.get(4);
+        System.out.println(i);
+        lfuCache.printNode();
+
     }
 }
