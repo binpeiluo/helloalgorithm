bfb2fe6373f65760384c5e46736a19c42899efc1
binpeiluo <1881975167@163.com>
2020-12-09 11:14:24 +0800

    构造二叉树系列 654. 最大二叉树

--- hellojike/src/com/luo/labuladong/mind/recurse/RecurseMain.java
+++ hellojike/src/com/luo/labuladong/mind/recurse/RecurseMain.java
@@ -108,6 +108,40 @@ public class RecurseMain {
         return root;
     }
 
+    /**
+     * 654. 最大二叉树
+     * 给定一个不含重复元素的整数数组。一个以此数组构建的最大二叉树定义如下：
+     * 二叉树的根是数组中的最大元素。
+     * 左子树是通过数组中最大值左边部分构造出的最大二叉树。
+     * 右子树是通过数组中最大值右边部分构造出的最大二叉树。
+     * 通过给定的数组构建最大二叉树，并且输出这个树的根节点。
+     *
+     * 构造二叉树首先在于构造本节点然后在构造子节点
+     * @param nums
+     * @return
+     */
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return buildMaxBinaryTree(nums,0,nums.length-1);
+    }
+
+    private TreeNode buildMaxBinaryTree(int[] nums,int left,int right){
+        if(left>right){
+            return null;
+        }
+//        首先构造根节点,然后在构造子节点
+        int maxIndex=left;
+        for (int i = left; i <=right; i++) {
+            maxIndex=nums[i]>nums[maxIndex]?i:maxIndex;
+        }
+        TreeNode root=new TreeNode(nums[maxIndex]);
+        TreeNode rootLeft=buildMaxBinaryTree(nums,left,maxIndex-1);
+        TreeNode rootRight=buildMaxBinaryTree(nums,maxIndex+1,right);
+        root.left=rootLeft;
+        root.right=rootRight;
+        return root;
+    }
+
+
 
 
 }
